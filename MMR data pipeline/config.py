38dc26e6595e22d894bdf07b498d7361536a6d80
makenasandra <makenawachira@gmail.com>
2020-09-30 05:17:08 +0300

    changing to db conn

--- MMR data pipeline/config.py
+++ MMR data pipeline/config.py
@@ -1,22 +1,9 @@
-'''
 import os
-from urllib.parse import quote_plus
 
-"""
-add the PASSWORD of database and UID to path.
-e.g export PASSWORD=pass123
-    export USERID=awesomeuser101
-"""
+#CONN_STR = os.environ.get('CONN_STR')
 
-CONN_STR = os.environ.get('CONN_STR')
-
-PARAMS = quote_plus(CONN_STR)
-#If 'TypeError: quote_from_bytes() expected bytes' occurs try
-#PARAMS = quote_plus(str(CONN_STR))
-
-# Azure MySQL
-DATABASE_URI = 'mssql+pyodbc:///?odbc_connect={}'.format(PARAMS)    # Construct as database uri
-
-# Local Postgresql
-# DATABASE_URI = 'postgres+psycopg2://postgres:postgres@localhost:5432/data'
-'''
+server = 'health-assist.database.windows.net'
+database = 'MmrSensorData'
+username = 'mmruser'
+password = 'Metadata123'
+DATABASE_URI = 'DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password
