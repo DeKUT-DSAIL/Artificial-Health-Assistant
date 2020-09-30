c7bb670ec5c71117d25d9c86bc2207edb9d80118
makenasandra <makenawachira@gmail.com>
2020-09-30 05:41:47 +0300

    changed db conn

--- MMR data pipeline/main.py
+++ MMR data pipeline/main.py
@@ -1,47 +1,64 @@
-from pymetawear.client import MetaWearClient
+#from pymetawear.client import MetaWearClient
+#from model import BodyAccX, BodyAccY, BodyAccZ, BodyGyroX, BodyGyroY, BodyGyroZ
 
-from model import BodyAccX, BodyAccY, BodyAccZ, BodyGyroX, BodyGyroY, BodyGyroZ
-from utils import Session, stream_acc_data, stream_gyr_data
+import pandas as pd
+from pymetawear.discover import select_device
+from utils import conn, stream_data
 
 
-def record_data(label: str, device: MetaWearClient, rounds: int) -> None:
+def record_data(label: str, device: MetaWearClient) -> None:
     """
     This function commits the data streamed to the database
     :param label:
     :param device:
-    :param rounds:
     :return None:
     """
-    s = Session()
-
-    print(f"Logging and adding data for {label} to database")
-    print(f"(Approx. {rounds * 27} seconds)")
-    for i in range(rounds):
-        print(f'Round {i + 1}')
-        acc, gyr = stream_acc_data(device=device), stream_gyr_data(device=device)
-        print("Finished!")
-
-        gx = gyr[0].replace('[', '').replace(']', '')
-        gy = gyr[1].replace('[', '').replace(']', '')
-        gz = gyr[2].replace('[', '').replace(']', '')
-
-        ax = acc[0].replace('[', '').replace(']', '')
-        ay = acc[1].replace('[', '').replace(']', '')
-        az = acc[2].replace('[', '').replace(']', '')
-
-        s.add(BodyAccX(row_data=ax, label=label))
-        s.add(BodyAccY(row_data=ay, label=label))
-        s.add(BodyAccZ(row_data=az, label=label))
-
-        s.add(BodyGyroX(row_data=gx, label=label))
-        s.add(BodyGyroY(row_data=gy, label=label))
-        s.add(BodyGyroZ(row_data=gz, label=label))
-
-    print(f"Committing {label} of {rounds} Round(s) data to database...")
-
-    s.commit()
+    print(f"Logging data for {label}")
+    acc, gyr = stream_data(device=device)
     print("Finished!")
-    s.close()
+
+    #Removing unnecessary columns 
+    acc_df = acc(['elapsed (s)', 'time (06:00)'], axis = 1)
+    gyr_df = gyr(['elapsed (s)', 'time (06:00)'], axis = 1)
+
+    #Renaming columns
+    acc_df.rename(columns = {'epoch (ms)': 'Epoch', 'x-axis (g)':'X_axis', 'y-axis (g)': 'Y_axis', 'z-axis (g)': 'Z_axis'}, inplace = True) 
+    gyr_df.rename(columns = {'epoch (ms)': 'Epoch', 'x-axis (deg/s)':'X_axis', 'y-axis (deg/s)': 'Y_axis', 'z-axis (deg/s)': 'Z_axis'}, inplace = True) 
+
+    #Add activity label
+    acc_df =acc_df.assign(Label = label)
+    gyr_df = gyr_df.assign(Label = label)
+    
+    # Create Table if one doesnt already exist
+    cursor.execute('''
+    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
+           WHERE TABLE_NAME = N'body_acc_all')
+    BEGIN
+     CREATE TABLE body_acc_all (Epoch bigint, X_axis float, Y_axis float, Z_axis float, Label varchar(50))
+    END '''
+    )
+    
+    print(f"Committing {label} data to database...")
+    
+    # Insert DataFrame to Table
+    for row in acc_df.itertuples():
+        cursor.execute('''
+                    INSERT INTO MmrSensorData.dbo.body_acc_all (Epoch, X_axis, Y_axis, Z_axis, Label)
+                    VALUES (?,?,?,?,?)
+                    ''',
+                    row.Epoch, 
+                    row.X_axis,
+                    row.Y_axis,
+                    row.Z_axis,
+                    row.Label
+                    )
+    
+    conn.commit()
+    cursor.close()
+    
+    print("Finished!")
+    
+    cursor.close()
 
 
 def run() -> None:
@@ -50,51 +67,49 @@ def run() -> None:
     :return None:
     """
     # Create a MetaWear device
-    d = MetaWearClient('EE:50:E7:BF:21:83')  # Substitute with your MAC
+    address = select_device()
+    d = MetaWearClient(str(address))
 
     proceed = True
 
+    prompt = """
+    \nChoose a number for an exercise below
+    1. Walking
+    2. Sitting
+    3. Matching
+    4. Body Stretch(Arms)
+    """
+
     while proceed:
-        exercise = int(input(
-            "\nChoose a number for an exercise below\n"
-            "1. Jumping Jacks\n"
-            "2. Squats\n"
-            "3. Jogs\n"
-            "4. Body Stretch(Arms)\n"
-        ))
+        exercise = int(input(prompt))
 
         while exercise not in [1, 2, 3, 4]:
-            exercise = int(input(
-                "\nChoose a number for an exercise below\n"
-                "1. Jumping Jacks\n"
-                "2. Squats\n"
-                "3. Jogs\n"
-                "4. Body Stretch(Arms)\n"
-            ))
+            exercise = int(input(prompt))
 
         if exercise == 1:
-            action = 'jumping jacks'
+            action = 'walking'
         elif exercise == 2:
-            action = 'squats'
+            action = 'sitting'
         elif exercise == 3:
-            action = 'jogs'
+            action = 'matching'
         else:
             action = 'body stretch'
 
-        rounds = int(input(
-            f"Enter number of Rounds You'll do {action}: "
-        ))
+        time_ = int(input(f"\nEnter Seconds You'll do {action}:\n"))
+        # time_ = 60
 
         # Start to stream and record data
         try:
-            record_data(label=action, device=d, rounds=rounds)
+            record_data(label=action, device=d, time_=time_)
             choice = input('Continue or exit\n1. Continue\n2. Exit\n')
             if choice != '1':
+                print("Exiting...")
                 proceed = False
         except Exception as message:
             print("Commit to database failed terribly due to\n", message)
             choice = input('Repeat or exit\n1. Repeat\n2. Exit\n')
             if choice != '1':
+                print("Exiting...")
                 proceed = False
 
         # Disconnect device
--- MMR data pipeline/utils.py
+++ MMR data pipeline/utils.py
@@ -1,80 +1,89 @@
-from time import sleep
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import sessionmaker
+#from model import Base
 
+import pyodbc
+import pandas as pd
+from time import sleep
 from mbientlab.metawear import libmetawear
 from pymetawear.client import MetaWearClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from model import Base
 
 from config import DATABASE_URI
 
-engine = create_engine(DATABASE_URI)
-Session = sessionmaker(bind=engine)
+# Connect to SQL Server
+conn = pyodbc.connect(DATABASE_URI)
+cursor = conn.cursor()
+
+#engine = create_engine(DATABASE_URI)
+#Session = sessionmaker(bind=engine)
 
 
-def stream_acc_data(device: MetaWearClient, data_rate: float = 50.0, acc_data_range: float = 16.0):
+
+# Print iterations progress
+def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="\r"):
     """
-    This function streams accelerometer data
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    print(f'\r{prefix}\t |{bar}| {percent}% {suffix}', end=print_end)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+        
+        
+def stream_data(device: MetaWearClient, data_rate: float = 25.0, acc_data_range: float = 16.0,
+                gyr_data_range: int = 500):
+    """
+    This function streams accelerometer and gyroscope data
     :param device: MetaWearClient
     :param data_rate: float
     :param acc_data_range: float
-    :return accelerometer tuples of x, y, z axis:
+    :param gyr_data_range: int
+    :return accelerometer and gyroscope Dataframes:
     """
     acc_data_points = []
-
-    # Set data rate and measuring range
-    device.accelerometer.set_settings(data_rate=data_rate, data_range=acc_data_range)
-
-    def acc_callback(data_struct):
-        """Handle a (epoch, (x,y,z)) data tuple."""
-        if len(acc_data_points) <= 199:
-            acc_data_points.append(data_struct)
-
-    # Enable notifications and register a callback for them.
-    device.accelerometer.notifications(callback=acc_callback)
-
-    while len(acc_data_points) < 200:
-        sleep(0.02)
-    device.accelerometer.notifications()
-
-    acc = (str([i['value'].x for i in acc_data_points]), str([j['value'].y for j in acc_data_points]),
-           str([k['value'].z for k in acc_data_points]))
-
-    return acc
-
-
-def stream_gyr_data(device: MetaWearClient, data_rate: float = 50.0, gyr_data_range: int = 500):
-    """
-    This function streams gyroscope data
-    :param device: MetaWearClient
-    :param data_rate: float
-    :param gyr_data_range: int
-    :return gyroscope tuples of x, y, z axis:
-    """
     gyr_data_points = []
 
     # Set data rate and measuring range
+    device.accelerometer.set_settings(data_rate=data_rate, data_range=acc_data_range)
     device.gyroscope.set_settings(data_rate=data_rate, data_range=gyr_data_range)
 
-    def gyr_callback(data_struct):
-        """Handle a (epoch, (x,y,z)) data tuple."""
-        if len(gyr_data_points) <= 199:
-            gyr_data_points.append(data_struct)
 
     # Enable notifications and register a callback for them.
-    device.gyroscope.notifications(callback=gyr_callback)
-
-    while len(gyr_data_points) < 200:
+    device.accelerometer.notifications(callback=lambda data: acc_data_points.append(data))
+    device.gyroscope.notifications(callback=lambda data: gyr_data_points.append(data))
+    
+    print_progress_bar(0, time_, prefix='Collecting Data:', suffix='Complete', length=30)
+    while counter <= time_:
         sleep(0.02)
+        print_progress_bar(counter, time_, prefix='Collecting Data:', suffix='Complete', length=30)
+        counter += 0.02
+    print_progress_bar(time_, time_, prefix='Collecting Data:', suffix='Complete', length=30)
+
+    device.accelerometer.notifications()
     device.gyroscope.notifications()
 
-    gyr = (str([i['value'].x for i in gyr_data_points]), str([j['value'].y for j in gyr_data_points]),
-           str([k['value'].z for k in gyr_data_points]))
 
-    return gyr
+    acc = pd.DataFrame(acc_data_points)
+
+    gyr = pd.DataFrame(gyr_data_points)
+
+    return acc, gyr
 
 
+'''
 def recreate_database() -> None:
     """
     his function creates tables in a database in None exist
@@ -86,7 +95,7 @@ def recreate_database() -> None:
     except:
         pass
     Base.metadata.create_all(engine)
-
+'''
 
 def reset(device) -> None:
     """
